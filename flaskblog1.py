a3ebd6c6c207b0d3b339b0f96995d46b2f7e4008
bchilds <Very5246>
2018-10-17 18:52:26 -0500

    week1

---
+++ flaskblog1.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+favSnacks = ['Pizza', 'Tacos', 'Salsa', 'Hummus']
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/lab")
+def lab():
+    return render_template('lab.html', posts=favSnacks, title='Bethany Childs')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
